36e02d14be7cc593c875520fccaec69d7ae5dbf4
CHN-STUDENT <CHN-STUDENT@outlook.com>
2019-05-26 18:11:50 +0800

    just start

---
+++ E-HAUT/Service/Crypto.swift
@@ -0,0 +1,63 @@
+//
+//  Crypto.swift
+//  E-HAUT
+//
+//  Created by zh on 2019/5/26.
+//  Copyright © 2019 ehaut. All rights reserved.
+//
+
+import Foundation
+
+class Crypto {
+    
+    static func usernameEncrypt(username : String) -> String {
+        var user : String = "{SRUN3}\r\n"
+        
+        for each in username {
+            let e = each.asciiValue + 4
+            user += e.asciiToString
+        }
+        
+        return user
+    }
+    
+    static func passwordEncrypt(password : String, passwordKey : String ) -> String {
+        var passwd : String = ""
+        let passwordKeyLength = passwordKey.count
+        let key = Array(passwordKey)
+        var i = 0
+        for each in password {
+            var ki: Int = (each.asciiValue) ^ ((key[passwordKeyLength - i % passwordKeyLength - 1]).asciiValue)
+            var _l: Int = Int((ki & 0x0f) + 0x36)
+            var _h: Int = Int((ki >> 4 & 0x0f) + 0x63)
+            if(i % 2 == 0) {
+                passwd += _l.asciiToString + _h.asciiToString
+            } else {
+                passwd += _h.asciiToString + _l.asciiToString
+            }
+            i += 1
+        }
+        
+        return passwd
+    }
+}
+
+
+extension Character {
+    var asciiValue: Int {
+        get {
+            let s = String(self).unicodeScalars
+            return Int(s[s.startIndex].value)
+        }
+    }
+}
+
+extension Int {
+    var asciiToString: String
+    {
+        get {
+            return String(UnicodeScalar(UInt8(self)))
+        }
+    }
+}
+
---
+++ E-HAUT/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  E-HAUT
+//
+//  Created by zh on 2019/5/26.
+//  Copyright © 2019 ehaut. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       
+    }
+    
+
+}
+
