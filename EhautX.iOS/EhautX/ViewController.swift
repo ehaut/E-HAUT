74d8b52106aac1b47800ebd34d2ac9eb8de54588
zengxs <i@zengxs.com>
2019-08-07 17:47:31 +0800

    Add iOS Support

---
+++ EhautX.iOS/EhautX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EhautX
+//
+//  Created by EhautTeam on 2019/8/7.
+//  Copyright © 2019 Ehaut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
